15f89946ec99752fe679d5904dbd900385d36cb4
ravif6 <47972432+ravif6@users.noreply.github.com>
2023-05-07 11:38:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { FetchingData } from './Components/FetchingData';
+// import { FetchingData } from './Components/FetchingData';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <BrowserRouter>
